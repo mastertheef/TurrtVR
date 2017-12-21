482a2b9060ba603e0e37e25a6ab18f3f8e710e60
Mykola <m.shvydkyy@abtollc.com>
2017-12-21 13:01:35 +0200

    portal

---
+++ TurretVR/Assets/Scripts/InvisibleAffected.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InvisibleAffected : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Renderer[] renderers = GetComponentsInChildren<Renderer>();
+        Debug.Log(renderers.Length);
+        foreach(Renderer r in renderers)
+        {
+            r.material.renderQueue = 2503;
+        }
+	}
+}
---
+++ TurretVR/Assets/Shaders/InvisibleMask.shader
@@ -0,0 +1,12 @@
+﻿Shader "Custom/InvisibleMask" {
+  SubShader {
+    // draw after all opaque objects (queue = 2001):
+    Tags { "Queue"="Geometry+501" }
+	ColorMask 0
+	ZWrite On
+	
+    Pass {
+      
+    }
+  } 
+}
